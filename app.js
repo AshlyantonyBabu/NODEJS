b8490a6ef75f02f16c915fb214143555cc8d385a
AshlyantonyBabu <elsyantonybabu@gmail.com>
2019-08-05 11:50:53 +0530

    node

---
+++ app.js
@@ -0,0 +1,11 @@
+greeting=require("./greeting")
+greeting.heelo();
+greeting.heelobyee();
+
+var xx = function(){
+console.log("hello world");
+}
+xx();
+for(var i in greeting.langu){
+    console.log(greeting.langu[i]);
+}
---
+++ greeting.js
@@ -0,0 +1,13 @@
+exports.heelo=function(){
+        console.log("welcome.....")
+    }
+    exports.heelobyee=function(){
+        console.log("byeee.....")
+    }
+function greet(){
+    console.log("greetings.....")
+}
+exports. langu=["C","C++","Java"];
+   
+    
+
